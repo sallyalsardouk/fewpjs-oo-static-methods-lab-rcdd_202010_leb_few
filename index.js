491c2e2b669f90a3c1176457d8a5fea4f661e5ff
sally al sardouk <sally.sardouk98@gmail.com>
2020-12-11 18:40:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-class Formatter {
-  //add static methods here
+class Formatter{
+  static capitalize(str){
+  return str.charAt(0).toUpperCase() + str.slice(1)
+}
+static sanitize(str){
+  return str.replace(/[^A-Za-z0-9 '-]/g, '')
+}
+static titleize(str){
+  
+}
 }
