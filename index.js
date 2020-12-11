c2b91c2d3adc7413cc5b6b10058e60c77d417fd1
sally al sardouk <sally.sardouk98@gmail.com>
2020-12-11 18:43:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-class Formatter {
-  //add static methods here
+class Formatter{
+  static capitalize(str){
+  return str.charAt(0).toUpperCase() + str.slice(1)
+}
+static sanitize(str){
+  return str.replace(/[^A-Za-z0-9 '-]/g, '')
+}
+static titleize(str){
+    let exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    let result = [];
+    let array = str.split(" ");
+
+    for (let i = 0; i < array.length; i++) {
+      if (i === 0) {
+        result.push(this.capitalize(array[i]))
+      } else {
+        if (exceptions.includes(array[i])) {
+          result.push(array[i])
+        } else {
+          result.push(this.capitalize(array[i]))
+        }
+      }
+    }
+    return result.join(" ");
+  }
+
 }
